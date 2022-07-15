d00a79a96098ee63227d83ba46d163a5e111d0a7
firemachineFTW <61358182+firemachineFTW@users.noreply.github.com>
2022-07-14 19:42:42 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,103 @@
+const cors = require('cors');
+
+const express = require('express');
+const mysql = require('mysql');
+
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(cors())
+
+app.use(bodyParser.json());
+
+// MySql
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'firemachineFTW.23',
+  database: 'partes', 
+  insecureAuth : true
+});
+
+
+
+// Route
+app.get('/', (req, res) => {
+  res.send('Welcome to my API!');
+});
+
+// all customers
+app.get('/partes/', (req, res) => {
+  const sql = 'SELECT * FROM autopartes';
+
+  connection.query(sql, (error, results) => {
+    if (error) throw error;
+    if (results.length > 0) {
+      res.json(results);
+    } else {
+      res.send('Not result');
+    }
+  });
+});
+
+app.get('/partes/:id', (req, res) => {
+  const { id } = req.params;
+  const sql = `SELECT * FROM autopartes WHERE id = ${id}`;
+  connection.query(sql, (error, result) => {
+    if (error) throw error;
+
+    if (result.length > 0) {
+      res.json(result);
+    } else {
+      res.send('Not result');
+    }
+  });
+});
+
+app.post('/partes/', (req, res) => {
+  const sql = 'INSERT INTO autopartes SET ?';
+
+  const customerObj = {
+    nombre: req.body.nombre,
+    descripcion: req.body.descripcion,
+    stock: req.body.stock,
+    precio: req.body.precio
+  };
+
+  connection.query(sql, customerObj, error => {
+    if (error) throw error;
+    res.send('Pieza insertada :)');
+  });
+});
+
+app.put('/partes/:id', (req, res) => {
+  const { id } = req.params;
+  const { nombre, descripcion, stock, precio } = req.body;
+  const sql = `UPDATE autopartes SET nombre = '${nombre}', descripcion='${descripcion}', stock='${stock}', precio='${precio}'WHERE id =${id}`;
+
+  connection.query(sql, error => {
+    if (error) throw error;
+    res.send('Pieza modificada');
+  });
+});
+
+app.delete('/partes/:id', (req, res) => {
+  const { id } = req.params;
+  const sql = `DELETE FROM autopartes WHERE id= ${id}`;
+
+  connection.query(sql, error => {
+    if (error) throw error;
+    res.send('Pieza eliminada');
+  });
+});
+
+// Check connect
+connection.connect(error => {
+  if (error) throw error;
+  console.log('Database server running!');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
